efbc1b8751e79a3f9f1e4b9bf069a2b54ae370be
Christian Maino Vieytes <cam17@illinois.edu>
2023-03-27 13:40:08 -0400

    data documentation first commit

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' Test Data for qfs_scores
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `diet.data`
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{HQ1}{How many times per day, week, or month did you eat cold cereals?}
+#'   \item{HQ2}{How many times per day, week, or month did you use milk, either to drink or on cereal?}
+#'   \item{HQ2A}{What kind of milk did you use?}
+#'   \item{HQ3}{How many times per day, week, or month did you usually eat bacon or sausage, not including low fat, light, or turkey varieties?}
+#'   \item{HQ4}{How often did you eat hot dogs made of beef or pork?}
+#'   \item{HQ5}{How often did you eat whole grain bread including toast, rolls, and in sandwhiches? Whole grain breads include whole wheat, rye, oatmeal, and pumpernickel.}
+#'   \item{HQ6}{How often did you drink 100% fruit juice such as orange, grapefruit, apple, and grape juices?}
+#'   \item{HQ7}{How often did you eat fruit?}
+#'   \item{HQ8}{How often did you use regular fat salad dressing or mayonnaise, incudling on salad and sandwiches?}
+#'   \item{HQ9}{How often did you eat lettuce or green leafy salad, with or without other vegetables?}
+#'   \item{HQ10}{How often did you eat french fries, home fries, or hash brown potatoes?}
+#'   \item{HQ11}{How often did you eat other white potatoes? Count baked potatoes, boiled potatoes, mashed potatoes, and potato salad. Do not include yams or sweet potatoes.}
+#'   \item{HQ12}{How often did you eat cooked dried beans, such as refried beans, baked beans, bean soup, and pork and beans.}
+#'   \item{HQ13}{How often did you usually eat other vegetables}
+#'   \item{HQ14}{How many times per day, week, or month did you usually eat any kind of pasta? Count spaghetti, noodles, macaroni and cheese, pasta salad, rice noodles, soba, and any other kind of pasta.}
+#'   \item{HQ15}{How often did you eat peanuts, walnuts, seeds, or other nuts? Do not include peanut butter.}
+#'   \item{HQ16}{How often did you eat regular fat potato chips, tortilla chips, or corn chips? Do not include low-fat chips.}
+#' }
+#' @source <https://epi.grants.cancer.gov/diet/shortreg/instruments/multifactor-screener-in-open-self-report-version.pdf>
+"diet.data"
