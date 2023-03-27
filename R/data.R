067330ab49eb7f8fc594eb3cc7d5174eb495566a
Christian Maino Vieytes <cam17@illinois.edu>
2023-03-27 14:48:25 -0400

    error diet.data documentation

--- R/data.R
+++ R/data.R
@@ -1,28 +1,30 @@
 #' Test Data for qfs_scores
 #'
-#' A subset of data from the World Health Organization Global Tuberculosis
-#' Report ...
+#' Test data for examples in the qfs_scores function.
 #'
 #' @format ## `diet.data`
-#' A data frame with 7,240 rows and 60 columns:
+#' A data frame with 45 rows and 19 columns:
 #' \describe{
-#'   \item{HQ1}{How many times per day, week, or month did you eat cold cereals?}
-#'   \item{HQ2}{How many times per day, week, or month did you use milk, either to drink or on cereal?}
-#'   \item{HQ2A}{What kind of milk did you use?}
-#'   \item{HQ3}{How many times per day, week, or month did you usually eat bacon or sausage, not including low fat, light, or turkey varieties?}
-#'   \item{HQ4}{How often did you eat hot dogs made of beef or pork?}
-#'   \item{HQ5}{How often did you eat whole grain bread including toast, rolls, and in sandwhiches? Whole grain breads include whole wheat, rye, oatmeal, and pumpernickel.}
-#'   \item{HQ6}{How often did you drink 100% fruit juice such as orange, grapefruit, apple, and grape juices?}
-#'   \item{HQ7}{How often did you eat fruit?}
-#'   \item{HQ8}{How often did you use regular fat salad dressing or mayonnaise, incudling on salad and sandwiches?}
-#'   \item{HQ9}{How often did you eat lettuce or green leafy salad, with or without other vegetables?}
-#'   \item{HQ10}{How often did you eat french fries, home fries, or hash brown potatoes?}
-#'   \item{HQ11}{How often did you eat other white potatoes? Count baked potatoes, boiled potatoes, mashed potatoes, and potato salad. Do not include yams or sweet potatoes.}
-#'   \item{HQ12}{How often did you eat cooked dried beans, such as refried beans, baked beans, bean soup, and pork and beans.}
-#'   \item{HQ13}{How often did you usually eat other vegetables}
-#'   \item{HQ14}{How many times per day, week, or month did you usually eat any kind of pasta? Count spaghetti, noodles, macaroni and cheese, pasta salad, rice noodles, soba, and any other kind of pasta.}
-#'   \item{HQ15}{How often did you eat peanuts, walnuts, seeds, or other nuts? Do not include peanut butter.}
-#'   \item{HQ16}{How often did you eat regular fat potato chips, tortilla chips, or corn chips? Do not include low-fat chips.}
+#'   \item{HQ1}{Cold cereals frequency of consumption}
+#'   \item{HQ2}{Milk frequency of consumption}
+#'   \item{HQ2A}{Milk type}
+#'   \item{HQ3}{Bacon or sausage frequency of consumption}
+#'   \item{HQ4}{Beef or pork hot dogs frequency of consumption}
+#'   \item{HQ5}{Whole grain bread frequency of consumption}
+#'   \item{HQ6}{100% fruit juice frequency of consumption}
+#'   \item{HQ7}{Fruit frequency of consumption}
+#'   \item{HQ8}{Salad dressing or mayo frequency of consumption}
+#'   \item{HQ9}{Lettuce or green-leafy salad frequency of consumption}
+#'   \item{HQ10}{French fries frequency of consumption}
+#'   \item{HQ11}{White potato frequency of consumption}
+#'   \item{HQ12}{Beans frequency of consumption}
+#'   \item{HQ13}{Other vegetables frequency of consumption}
+#'   \item{HQ14}{Pasta frequency of consumption}
+#'   \item{HQ15}{Nuts frequency of consumption}
+#'   \item{HQ16}{Regular fat potato chips frequency of consumption}
+#'   \item{SEX}{Participant sex}
+#'   \item{AGE}{Participant age}
 #' }
-#' @source <https://epi.grants.cancer.gov/diet/shortreg/instruments/multifactor-screener-in-open-self-report-version.pdf>
+#'
+#' @source <https://epi.grants.cancer.gov/past-initiatives/open/multifactor/scoring.html>
 "diet.data"
